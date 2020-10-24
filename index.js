bb5907fb1e4348fdf96cdc05fff033f52e7d1313
user <user@rrcs-172-254-99-48.nyc.biz.rr.com>
2020-10-25 07:56:27 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const line = require('@line/bot-sdk');
+const PORT = process.env.PORT || 5000
+
+const config = {
+   channelAccessToken:process.env.ACCESS_TOKEN,
+   channelSecret:process.env.CHANNEL_SECRET
+};
+
+const client = new line.Client(config);
+
+app
+   .post('/hook',line.middleware(config),(req,res)=> lineBot(req,res))
+   .listen(PORT,()=>console.log(`Listening on ${PORT}`));
+
+   const lineBot = (req,res) => {
+    res.status(200).end();
+    const events = req.body.events;
+    const promises = [];
+    for(let i=0;i<events.length;i++){
+        const ev = events[i];
+        switch(ev.type){
+            case 'follow':
+                promises.push(greeting_follow(ev));
+                break;
+        }
+    }
+    Promise
+        .all(promises)
+        .then(console.log('all promises passed'))
+        .catch(e=>console.error(e.stack));
+ }
+
+ const greeting_follow = async (ev) => {
+    const profile = await client.getProfile(ev.source.userId);
+    return client.replyMessage(ev.replyToken,{
+        "type":"text",
+        "text":`${profile.displayName}さん、フォローありがとうございます\uDBC0\uDC04`
+    });
+ }
+
+ 
