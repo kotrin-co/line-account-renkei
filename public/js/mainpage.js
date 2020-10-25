a7eede5c2a07f8abc17512ce1cff313986b9703d
user <user@rrcs-172-254-99-48.nyc.biz.rr.com>
2020-10-25 15:28:23 +0900

    url

---
+++ public/js/mainpage.js
@@ -0,0 +1,11 @@
+// const param = new URL(location).search;
+// const splitParam = param.split('=');
+// const linkToken = splitParam[1];
+// document.getElementById('urlparam').innerHTML = linkToken;
+
+// const N=16
+// const nonce = btoa(String.fromCharCode(...crypto.getRandomValues(new Uint8Array(N)))).substring(0,N);
+// document.getElementById('nonce').innerHTML = nonce;
+
+// const target = document.getElementById('login-tag');
+// target.href = `https://access.line.me/dialog/bot/accountLink?linkToken=${linkToken}&nonce=${nonce}`;
