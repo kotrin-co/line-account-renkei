79b16bc0503b80fbbac3bd5e484f0e8d19b3f589
user <user@rrcs-172-254-99-48.nyc.biz.rr.com>
2020-10-25 10:14:03 +0900

    dom index.ejs

---
+++ public/js/main.js
@@ -0,0 +1,38 @@
+const divLogin = document.getElementById('login_area');
+
+const formElement = document.createElement('form');
+formElement.setAttribute('id','login');
+formElement.setAttribute('name','login_info');
+formElement.setAttribute('method','post');
+
+const div_form1 = document.createElement('div');
+
+const label_form1 = document.createElement('label');
+label_form1.setAttribute('class','label_id');
+label_form1.textContent = 'ログインID';
+div_form1.appendChild(label_form1);
+
+const input_form1 = document.createElement('input');
+input_form1.setAttribute('type','text');
+input_form1.setAttribute('class','id-input');
+input_form1.setAttribute('name','id');
+div_form1.appendChild(input_form1);
+
+const div_form2 = document.createElement('div');
+
+const label_form2 = document.createElement('label');
+label_form2.setAttribute('class','label_password');
+label_form2.textContent = 'パスワード';
+div_form2.appendChild(label_form2);
+
+const input_form2 = document.createElement('input');
+input_form2.setAttribute('type','password');
+input_form2.setAttribute('class','password-input');
+input_form2.setAttribute('name','password');
+div_form2.appendChild(input_form2);
+
+formElement.appendChild(div_form1);
+formElement.appendChild(div_form2);
+
+divLogin.appendChild(formElement);
+
