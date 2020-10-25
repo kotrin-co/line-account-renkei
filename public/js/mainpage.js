e3235876ee6a9ca187fac16d04ba087d5f3b5539
user <user@rrcs-172-254-99-48.nyc.biz.rr.com>
2020-10-25 22:58:42 +0900

    final

--- public/js/mainpage.js
+++ public/js/mainpage.js
@@ -9,13 +9,3 @@ const linkToken = splitParam[2];
 document.getElementById('text_id').innerHTML = id;
 document.getElementById('text_password').innerHTML = password;
 document.getElementById('text_linkToken').innerHTML = linkToken;
-
-// const linkToken = splitParam[1];
-// document.getElementById('urlparam').innerHTML = linkToken;
-
-// const N=16
-// const nonce = btoa(String.fromCharCode(...crypto.getRandomValues(new Uint8Array(N)))).substring(0,N);
-// document.getElementById('nonce').innerHTML = nonce;
-
-// const target = document.getElementById('login-tag');
-// target.href = `https://access.line.me/dialog/bot/accountLink?linkToken=${linkToken}&nonce=${nonce}`;
