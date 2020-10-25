7a077ecf9a73476f12534145ab2a83058dbd2fc0
user <user@rrcs-172-254-99-48.nyc.biz.rr.com>
2020-10-25 12:07:09 +0900

    api

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app
    .use(express.static(path.join(__dirname, 'public')))
    .set('views', path.join(__dirname, 'views'))
    .set('view engine', 'ejs')
+   .use(express.json())
    .use('/',router)
    .use('/api/users',usersRouter)
    .post('/hook',line.middleware(config),(req,res)=> lineBot(req,res))
