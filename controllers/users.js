73d3353bce3cd34dc0ae1e13a1f3ff8d7c862458
user <user@rrcs-172-254-99-48.nyc.biz.rr.com>
2020-11-04 23:57:53 +0900

    redirect

--- controllers/users.js
+++ controllers/users.js
@@ -59,7 +59,7 @@ module.exports = {
                             .then(response=>{
                                 console.log('insert into nonces 成功');
                                 console.log('linktoken nonce:',linkToken,nonce);
-                                // res.status(200).redirect(`https://access.line.me/dialog/bot/accountLink?linkToken=${linkToken}&nonce=${nonce}`);
+                                res.status(200).redirect(`https://access.line.me/dialog/bot/accountLink?linkToken=${linkToken}&nonce=${nonce}`);
                             })
                             .catch(e=>console.log(e));
                     }else{
