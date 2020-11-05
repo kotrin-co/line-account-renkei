c360ad6e582e4ebcaca0e525d8c9a959ccb1bb52
user <user@rrcs-172-254-99-48.nyc.biz.rr.com>
2020-11-05 23:06:02 +0900

    req.query request

--- controllers/link.js
+++ controllers/link.js
@@ -1,7 +1,22 @@
+const request = require('request-promise');
 
 module.exports = {
 
     accountLink: (req,res) => {
-        console.log('req.query:',req.query.line_uid);
+        const line_uid = req.query.line_uid;
+        const options = {
+            url:`https://api.line.me/v2/bot/user/${line_uid}/linkToken`,
+            method:'POST',
+            headers:{
+                'Authorization':'Bearer /hwe0EhoKLsy2P1ynqJOWH3TWytYYrqlO6w9cPiDVjdJwwx2NoPosK98vovYkAH5Xu1oqYvpY8Fmr6/kE3maBr/zjr7I4MQ1az2puov0vg0CWmNgCQSulsMJd0yOqR2ruchBI0Uwntg7fE8tCgdWDQdB04t89/1O/w1cDnyilFU='
+            }
+        }
+
+        request(options)
+            .then(body=>{
+                const parsedBody = JSON.parse(body);
+                console.log('parsedBody:',parsedBody);
+                console.log('linkToken:',parsedBody["linkToken"]);
+            });
     }
 }
