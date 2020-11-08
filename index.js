ef43ba6fc6d8d26b9ea20c88eb1f8a3435c3ff3b
user <user@rrcs-172-254-99-48.nyc.biz.rr.com>
2020-11-08 15:05:46 +0900

    get

--- index.js
+++ index.js
@@ -272,20 +272,32 @@ const accountLink = (ev) => {
 }
 
 const handlePostbackEvent = (ev) => {
-    const line_uid = ev.source.userId;
-    const options = {
-        url:`https://api.line.me/v2/bot/user/${line_uid}/linkToken`,
-        method:'POST',
-        headers:{
-            'Authorization':'Bearer /hwe0EhoKLsy2P1ynqJOWH3TWytYYrqlO6w9cPiDVjdJwwx2NoPosK98vovYkAH5Xu1oqYvpY8Fmr6/kE3maBr/zjr7I4MQ1az2puov0vg0CWmNgCQSulsMJd0yOqR2ruchBI0Uwntg7fE8tCgdWDQdB04t89/1O/w1cDnyilFU='
-        }
-    }
 
-    request(options)
-            .then(body=>{
-                const parsedBody = JSON.parse(body);
-                console.log('parsedBody:',parsedBody);
-                console.log('linkToken:',parsedBody["linkToken"]);
-                res.status(200).redirect(`https://linebot-account-renkei.herokuapp.com?linkToken=${parsedBody["linkToken"]}`)
-            });
+    if(ev.postback.data === 'login'){
+        const line_uid = ev.source.userId;
+        const options = {
+            url: `https://linebot-account-renkei.herokuapp.com/api/link?line_uid=${line_uid}`,
+            method:'GET'
+        }
+
+        request(options)
+            .then(body=>console.log('body:',body));
+
+        // const options = {
+        //     url:`https://api.line.me/v2/bot/user/${line_uid}/linkToken`,
+        //     method:'POST',
+        //     headers:{
+        //         'Authorization':'Bearer /hwe0EhoKLsy2P1ynqJOWH3TWytYYrqlO6w9cPiDVjdJwwx2NoPosK98vovYkAH5Xu1oqYvpY8Fmr6/kE3maBr/zjr7I4MQ1az2puov0vg0CWmNgCQSulsMJd0yOqR2ruchBI0Uwntg7fE8tCgdWDQdB04t89/1O/w1cDnyilFU='
+        //     }
+        // }
+
+        // request(options)
+        //         .then(body=>{
+        //             const parsedBody = JSON.parse(body);
+        //             console.log('parsedBody:',parsedBody);
+        //             console.log('linkToken:',parsedBody["linkToken"]);
+                    // res.status(200).redirect(`https://linebot-account-renkei.herokuapp.com?linkToken=${parsedBody["linkToken"]}`)
+                // });
+    }
+    
 }
